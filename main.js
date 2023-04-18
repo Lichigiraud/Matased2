0ea0128a011d0874f931169367e34a80068d5552
lichigiraud <lisandrogiraud@gmail.com>
2023-04-18 16:06:53 -0300

    matased2

---
+++ main.js
@@ -0,0 +1,118 @@
+
+let carrito = []
+const contenedor = document.querySelector('#contenedor')
+const carritoContenedor = document.querySelector('#carritoContenedor')
+const vaciarCarrito = document.querySelector('#vaciarCarrito')
+const precioTotal = document.querySelector ('#precioTotal')
+const procesarCompra = document.querySelector('#procesarCompra')
+
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    carrito = JSON.parse(localStorage.getItem('carrito')) ||  [ ] ;
+    mostrarCarrito()
+
+})
+
+stockProductos.forEach((prod)=>{
+    const {id, nombre, precio, desc, img, cantidad} = prod
+    contenedor.innerHTML +=`
+    <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="${img}" alt="imagen">
+        <div class="card-body">
+            <h5 class="card-title">${nombre}</h5>
+            <p class="card-text">Precio: $${precio}</p>
+            <p class="card-text">Descripcion: ${desc}</p>
+            <button onclick= "agregarProducto(${id})" class="btn btn-primary">Añadir al carrito</button>
+        </div>
+    </div>
+`
+
+})
+if (procesarCompra) {
+    procesarCompra.addEventListener("click", () => {
+        if (carrito.length === 0) {
+        Swal.fire({
+            title: "¡Tu carrito está vacio!",
+            text: "Compra algo para continuar con la compra",
+            icon: "error",
+            confirmButtonText: "Aceptar",
+        });
+        } else {
+        location.href = "compra.html";
+        }
+    });
+}
+
+
+vaciarCarrito.addEventListener('click', () => {
+    carrito.length = []
+    mostrarCarrito()
+})
+
+function agregarProducto(id){
+    
+    const existe = carrito.some(prod => prod.id === id)
+    
+    if(existe){
+        const prod = carrito.map(prod => {
+            if(prod.id === id){
+                prod.cantidad++
+            }
+        })
+    } else {
+        
+    const item = stockProductos.find((prod) => prod.id === id)
+    carrito.push(item)
+    }
+    mostrarCarrito()
+}
+
+const mostrarCarrito = () => {
+    const modalBody = document.querySelector(' .modal .modal-body ')
+
+    modalBody.innerHTML = ''
+    carrito.forEach((prod) => {
+        const {id, nombre, img, desc ,cantidad, precio} = prod
+        modalBody.innerHTML +=` 
+        <div class= "modal-contenedor">
+        <div>
+        <img class="img-fluid img-carrito" src="${img}"/>
+        </div>
+        <div>
+        <p>Producto: ${nombre}</p>
+        <p>Precio: $ ${precio}</p>
+        <p>Cantidad: ${cantidad}</p>
+        
+        <button onclick= "eliminarProducto(${id})" class="btn btn-danger">Eliminar producto</button>
+        </div>
+        
+        </div>
+        `
+    })
+    if(carrito.length === 0){
+        modalBody.innerHTML= `<p class= "text-center text primary parrafo">Tu carrito esta vacio
+        </p>`
+    }
+    
+    carritoContenedor.textContent = carrito.length
+
+    precioTotal.innerText = carrito.reduce((acc, prod)=> acc + prod.cantidad * prod.precio, 0)
+    
+    guardarStorage()
+
+}
+function eliminarProducto(id){
+    const bebidaId = id
+    carrito = carrito.filter ((bebida) => bebida.id !== bebidaId )
+    mostrarCarrito()
+}
+
+function guardarStorage(){
+    localStorage.setItem("carrito",JSON.stringify(carrito))
+}
+function procesarPedido(){
+    carrito.forEach((prod)=>{
+        const listaCompra = document.querySelector('#lista-compra tbody')
+    })
+
+}
---
+++ stock.js
@@ -0,0 +1,58 @@
+const stockProductos = [
+    {
+        nombre: "Fernet Branca", 
+        precio: 2000,
+        img: "Assets/FernetBranca.jpg",
+        id: 1,
+        cantidad: 1,
+        desc: "Aperitivo a base de hierbas"
+    },
+    {
+        nombre: "Fernet Buhero Negro", 
+        precio: 1600,
+        img: "Assets/FernetBN.jpg",
+        id: 2,
+        cantidad: 1,
+        desc: "Aperitivo a base de hierbas"
+    },
+    {
+        nombre: "Vodka Smirnoff", 
+        precio: 1750,
+        img: "Assets/vodka.jpg",
+        id: 3,
+        cantidad: 1,
+        desc: "Bebida destilada, a base de alcohol y agua"
+    },
+    {
+        nombre: "Vino Termidor", 
+        precio: 725,
+        img: "Assets/termidor.jpg",
+        id: 4,
+        cantidad: 1,
+        desc:"Bebida hecha de uva, mediante la fermentación alcohólica de su mosto o zumo"
+    },
+    {
+        nombre: "Jaggermeister",
+        precio: 7000,
+        img: "Assets/jagger.jpg",
+        id: 5,
+        cantidad: 1,
+        desc:"Jägermeister es un licor de hierbas endulzado, pero con un toque amargo"
+    },
+    {
+        nombre: "Gin Bombay",
+        precio: 6000,
+        img: "Assets/bombay.jpg", 
+        id: 6,
+        cantidad: 1,
+        desc:"Estilo london dry"
+    },
+    {
+        nombre: "Cerveza Andes rubia X6" ,
+        precio: 1500,
+        img: "Assets/andes.jpg",
+        id: 7,
+        cantidad: 1,
+        desc:"Bebida alcohólica, no destilada, de sabor amargo"
+    } 
+]; 
